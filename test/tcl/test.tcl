cdfd25eb419545ab091e901cf88a35eadf279af5
David Muse <david.muse@firstworks.com>
2005-07-25 01:29:55 +0000

    *** empty log message ***

--- test/tcl/test.tcl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/tclsh
-
-load ../../src/api/tcl/sqlrelay.so sqlrelay
-
-set con [sqlrcon -server localhost -port 8009 -user oracle8test -password oracle8test]
-set cur [$con sqlrcur]
-
-$cur sendQuery "select * from testtable"
