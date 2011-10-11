0ee1110ea58765ff42b069d618cbfaa5b571948c
David Muse <david.muse@firstworks.com>
2011-10-11 08:15:31 +0000

    *** empty log message ***

---
+++ src/api/c++/src/cursor/getlist.C
@@ -0,0 +1,78 @@
+// Copyright (c) 1999-2011  David Muse
+// See the file COPYING for more information
+
+#include <config.h>
+#include <sqlrelay/sqlrclient.h>
+#include <defines.h>
+
+bool sqlrcursor::getDbList(const char *wild) {
+	return getList(GETDBLIST,NULL,wild);
+}
+
+bool sqlrcursor::getTableList(const char *wild) {
+	return getList(GETTABLELIST,NULL,wild);
+}
+
+bool sqlrcursor::getColumnList(const char *table, const char *wild) {
+	return getList(GETCOLUMNLIST,(table)?table:"",wild);
+}
+
+bool sqlrcursor::getList(uint16_t command,
+			const char *table, const char *wild) {
+
+	reexecute=false;
+	validatebinds=false;
+	resumed=false;
+	clearVariables();
+
+	if (!endofresultset) {
+		abortResultSet();
+	}
+	clearResultSet();
+
+	if (!sqlrc->openSession()) {
+		return false;
+	}
+
+	cached=false;
+	endofresultset=false;
+
+	// tell the server we want to get a db list
+	sqlrc->cs->write(command);
+
+	// tell the server whether we'll need a cursor or not
+	sendCursorStatus();
+
+	// send the table parameter
+	uint32_t	len=0;
+	if (table) {
+		len=charstring::length(table);
+		sqlrc->cs->write(len);
+		sqlrc->cs->write(table,len);
+	}
+
+	// send the wild parameter
+	len=charstring::length(wild);
+	sqlrc->cs->write(len);
+	sqlrc->cs->write(wild,len);
+
+	sqlrc->flushWriteBuffer();
+
+	// process the result set
+	bool	retval=true;
+	if (rsbuffersize) {
+		if (!processResultSet(false,rsbuffersize-1)) {
+			retval=false;
+		}
+	} else {
+		if (!processResultSet(true,0)) {
+			retval=false;
+		}
+	}
+
+	// set up not to re-execute the same query if executeQuery is called
+	// again before calling prepareQuery on a new query
+	reexecute=false;
+
+	return retval;
+}
---
+++ src/connection/sqlrconnection/getlist.C
@@ -0,0 +1,161 @@
+// Copyright (c) 1999-2011  David Muse
+// See the file COPYING for more information
+
+#include <sqlrconnection.h>
+
+bool sqlrconnection_svr::getDbListCommand(sqlrcursor_svr *cursor) {
+	return getListCommand(cursor,0,false);
+}
+
+bool sqlrconnection_svr::getTableListCommand(sqlrcursor_svr *cursor) {
+	return getListCommand(cursor,1,false);
+}
+
+bool sqlrconnection_svr::getColumnListCommand(sqlrcursor_svr *cursor) {
+	return getListCommand(cursor,2,false);
+}
+
+bool sqlrconnection_svr::getListCommand(sqlrcursor_svr *cursor,
+						int which, bool gettable) {
+
+ 	dbgfile.debugPrint("connection",2,"getting list command");
+
+	// get length of wild parameter
+	uint32_t	wildlen;
+	if (clientsock->read(&wildlen,idleclienttimeout,0)!=sizeof(uint32_t)) {
+		dbgfile.debugPrint("connection",2,
+			"get list failed: client sent bad wild length");
+		return false;
+	}
+
+	// bounds checking
+	if (wildlen>maxquerysize) {
+		dbgfile.debugPrint("connection",2,
+			"get list failed: client sent bad wild length");
+		return false;
+	}
+
+	// read the wild parameter into the buffer
+	char	*wild=new char[wildlen+1];
+	if ((uint32_t)(clientsock->read(wild,wildlen,
+					idleclienttimeout,0))!=wildlen) {
+		dbgfile.debugPrint("connection",2,
+			"get list failed: client sent short wild parameter");
+		return false;
+	}
+	wild[wildlen]='\0';
+
+	// read the table parameter into the buffer
+	char	*table=NULL;
+	if (gettable) {
+
+		// get length of table parameter
+		uint32_t	tablelen;
+		if (clientsock->read(&tablelen,
+				idleclienttimeout,0)!=sizeof(uint32_t)) {
+			dbgfile.debugPrint("connection",2,
+				"get list failed: "
+				"client sent bad table length");
+			return false;
+		}
+
+		// bounds checking
+		if (tablelen>maxquerysize) {
+			dbgfile.debugPrint("connection",2,
+				"get list failed: "
+				"client sent bad table length");
+			return false;
+		}
+
+		// read the table parameter into the buffer
+		table=new char[tablelen+1];
+		if ((uint32_t)(clientsock->read(table,tablelen,
+					idleclienttimeout,0))!=tablelen) {
+			dbgfile.debugPrint("connection",2,
+				"get list failed: "
+				"client sent short table parameter");
+			return false;
+		}
+		table[tablelen]='\0';
+	}
+
+	// build the appropriate query
+	const char	*query=NULL;
+	switch (which) {
+		case 0:
+			query=getDbListQuery(charstring::length(wild));
+			break;
+		case 1:
+			query=getTableListQuery(charstring::length(wild));
+			break;
+		case 2:
+			query=getColumnListQuery(charstring::length(wild));
+			break;
+	}
+	buildListQuery(cursor,query,wild,table);
+	delete[] wild;
+	delete[] table;
+
+	// set the values that we won'tget from the client
+	cursor->inbindcount=0;
+	cursor->outbindcount=0;
+	sendcolumninfo=SEND_COLUMN_INFO;
+
+	// run it like a normal query, but don't request the query,
+	// binds or column info status from the client
+	return newQueryInternal(cursor,false);
+}
+
+bool sqlrconnection_svr::buildListQuery(sqlrcursor_svr *cursor,
+						const char *query,
+						const char *wild,
+						const char *table) {
+
+	// clean up buffers to avoid SQL injection
+	stringbuffer	wildbuf;
+	escapeParameter(&wildbuf,wild);
+	stringbuffer	tablebuf;
+	escapeParameter(&tablebuf,table);
+
+	// bounds checking
+	cursor->querylength=charstring::length(query)+
+					wildbuf.getStringLength()+
+					tablebuf.getStringLength()+1;
+	if (cursor->querylength>maxquerysize) {
+		return false;
+	}
+
+	// fill the query buffer and update the length
+	snprintf(cursor->querybuffer,cursor->querylength,
+			query,wildbuf.getString(),tablebuf.getString());
+	cursor->querylength=charstring::length(cursor->querybuffer);
+	return true;
+}
+
+void sqlrconnection_svr::escapeParameter(stringbuffer *buffer,
+						const char *parameter) {
+
+	if (!parameter) {
+		return;
+	}
+
+	// escape single quotes and slashes
+	for (const char *ptr=parameter; *ptr; ptr++) {
+		if (*ptr=='\'' || *ptr=='\\') {
+			buffer->append('\\');
+		}
+		buffer->append(*ptr);
+	}
+}
+
+const char *sqlrconnection_svr::getDbListQuery(bool wild) {
+	return "select 1";
+}
+
+const char *sqlrconnection_svr::getTableListQuery(bool wild) {
+	return "select 1";
+}
+
+const char *sqlrconnection_svr::getColumnListQuery(bool wild) {
+	return "select 1";
+}
