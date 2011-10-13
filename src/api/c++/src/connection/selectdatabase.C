dfa844f7555066700ec6b97ac8f954fa45a2ed35
David Muse <david.muse@firstworks.com>
2011-10-13 07:33:43 +0000

    *** empty log message ***

---
+++ src/api/c++/src/connection/selectdatabase.C
@@ -0,0 +1,40 @@
+// Copyright (c) 1999-2011  David Muse
+// See the file COPYING for more information
+
+#include <sqlrelay/sqlrclient.h>
+#include <defines.h>
+
+bool sqlrconnection::selectDatabase(const char *database) {
+
+	if (!openSession()) {
+		return 0;
+	}
+
+	if (debug) {
+		debugPreStart();
+		debugPrint("Pinging...");
+		debugPrint("\n");
+		debugPreEnd();
+	}
+
+	// tell the server we want to select a db
+	cs->write((uint16_t)SELECT_DATABASE);
+
+	// send the database name
+	uint32_t	len=charstring::length(database);
+	cs->write(len);
+	if (len) {
+		cs->write(database,len);
+	}
+
+	flushWriteBuffer();
+
+	// get the result
+	bool	result;
+	if (cs->read(&result)!=sizeof(bool)) {
+		setError("Failed to select database.\n "
+				"A network error may have ocurred.");
+		return false;
+	}
+	return result;
+}
---
+++ src/connection/sqlrconnection/selectdatabase.C
@@ -0,0 +1,87 @@
+// Copyright (c) 1999-2001  David Muse
+// See the file COPYING for more information
+
+#include <sqlrconnection.h>
+
+void sqlrconnection_svr::selectDatabaseCommand() {
+
+	dbgfile.debugPrint("connection",1,"select database");
+
+	// get length of db parameter
+	uint32_t	dblen;
+	if (clientsock->read(&dblen,idleclienttimeout,0)!=sizeof(uint32_t)) {
+		dbgfile.debugPrint("connection",2,
+			"get list failed: client sent bad db length");
+		clientsock->write(false);
+		return;
+	}
+
+	// bounds checking
+	if (dblen>maxquerysize) {
+		dbgfile.debugPrint("connection",2,
+			"get list failed: client sent bad db length");
+		clientsock->write(false);
+		return;
+	}
+
+	// read the db parameter into the buffer
+	char	*db=new char[dblen+1];
+	if (dblen) {
+		if ((uint32_t)(clientsock->read(db,dblen,
+					idleclienttimeout,0))!=dblen) {
+			dbgfile.debugPrint("connection",2,
+				"get list failed: "
+				"client sent short db parameter");
+			clientsock->write(false);
+			return;
+		}
+	}
+	db[dblen]='\0';
+	
+	// select the db and send back the result
+	clientsock->write(selectDatabase(db));
+
+	// send back the result
+	flushWriteBuffer();
+
+	return;
+}
+
+bool sqlrconnection_svr::selectDatabase(const char *database) {
+
+	// get the select database query base
+	const char	*sdquerybase=selectDatabaseQuery();
+
+	// bounds checking
+	int		sdquerylen=charstring::length(sdquerybase)+
+					charstring::length(database)+1;
+	if (sdquerylen>maxquerysize) {
+		dbgfile.debugPrint("connection",2,
+			"get list failed: client sent bad db length");
+		return false;
+	}
+
+	// create the select database query
+	char	*sdquery=new char[sdquerylen];
+	snprintf(sdquery,sdquerylen,sdquerybase,database);
+	sdquerylen=charstring::length(sdquery);
+
+	sqlrcursor_svr	*sdcur=initCursorUpdateStats();
+	// since we're creating a new cursor for this, make sure it can't
+	// have an ID that might already exist
+	bool	retval=false;
+	if (sdcur->openCursorInternal(cursorcount+1) &&
+		sdcur->prepareQuery(sdquery,sdquerylen) &&
+		executeQueryUpdateStats(sdcur,sdquery,sdquerylen,true)) {
+		sdcur->cleanUpData(true,true);
+		retval=true;
+	}
+	delete[] sdquery;
+	sdcur->closeCursor();
+	deleteCursorUpdateStats(sdcur);
+	return retval;
+}
+
+const char *sqlrconnection_svr::selectDatabaseQuery() {
+	return "use %s";
+}
