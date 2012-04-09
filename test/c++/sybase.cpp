eb0c63146f34739fe22d46a54fa8133d7e91be80
David Muse <david.muse@firstworks.com>
2012-04-09 04:39:28 +0000

    *** empty log message ***

--- test/c++/sybase.cpp
+++ test/c++/sybase.cpp
@@ -261,6 +261,7 @@ int	main(int argc, char **argv) {
 	printf("\n");
 
 	printf("SELECT: \n");
+con->debugOn();
 	checkSuccess(cur->sendQuery("select * from testtable order by testint"),1);
 	printf("\n");
 
