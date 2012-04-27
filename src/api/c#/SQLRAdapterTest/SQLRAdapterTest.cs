1b5abfddc68d22c70210c1f8ad1f0c0f09b4ad8a
David Muse <david.muse@firstworks.com>
2012-04-27 03:51:57 +0000

    #733 added test

---
+++ src/api/c#/SQLRAdapterTest/SQLRAdapterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SQLRClient;
+
+namespace SQLRClientTest
+{
+    class SQLRAdapterTest
+    {
+        public static void Main(string[] args)
+        {
+            SQLRelayConnection sqlrcon = new SQLRelayConnection("host=fedora;port=9000;socket=;user=test;password=test;retrytime=0;tries=1");
+
+            sqlrcon.Open();
+
+            SQLRelayCommand sqlrcom = new SQLRelayCommand("select 1 from dual", sqlrcon);
+            sqlrcom.ExecuteNonQuery();
+
+            sqlrcon.Close();
+        }
+    }
+}
