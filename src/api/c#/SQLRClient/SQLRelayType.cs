9f9efcc33321415ea369301f32e875cbf8882e3b
David Muse <david.muse@firstworks.com>
2012-05-12 00:43:12 +0000

    #880 I added the SQLRelayType class for clobs, blobs and cursors

---
+++ src/api/c#/SQLRClient/SQLRelayType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLRClient
+{
+    [Serializable]
+    public enum SQLRelayType
+    {
+        Object = 0,
+        Clob,
+        Blob,
+        Cursor
+    }
+}
