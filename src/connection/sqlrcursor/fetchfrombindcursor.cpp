95b44f45ee0567756389e08dc0fbb8d39e0c50cf
David Muse <david.muse@firstworks.com>
2012-11-09 04:30:53 +0000

    *** empty log message ***

---
+++ src/connection/sqlrcursor/fetchfrombindcursor.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 1999-2012  David Muse
+// See the file COPYING for more information
+
+#include <sqlrconnection.h>
+
+bool sqlrcursor_svr::fetchFromBindCursor() {
+	// by default, do nothing...
+	return true;
+}
