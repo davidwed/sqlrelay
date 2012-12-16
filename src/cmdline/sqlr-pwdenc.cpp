2b688a81c2193309e27f3e56eccd6ee873a2b992
David Muse <david.muse@firstworks.com>
2012-12-16 01:38:05 +0000

    #1400 added sqlr-pwdenc

---
+++ src/cmdline/sqlr-pwdenc.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2012  David Muse
+// See the file COPYING for more information
+
+#include <config.h>
+#include <defaults.h>
+
+#include <rudiments/commandline.h>
+#include <rudiments/process.h>
+#include <sqlrconfigfile.h>
+#include <sqlrpwdencs.h>
+
+#include <stdio.h>
+
+#ifdef RUDIMENTS_NAMESPACE
+using namespace rudiments;
+#endif
+
+int main(int argc, const char **argv) {
+
+	#include <version.h>
+
+	// get the command line arguments
+	commandline	cmdline(argc,argv);
+	const char	*config=cmdline.getValue("-config");
+	if (!(config && config[0])) {
+		config=DEFAULT_CONFIG_FILE;
+	}
+	const char	*id=cmdline.getValue("-id");
+	const char	*pwdencid=cmdline.getValue("-pwdencid");
+	const char	*password=cmdline.getValue("-password");
+
+	// sanity check and usage message
+	if (!charstring::length(id) ||
+		!charstring::length(pwdencid) ||
+		!charstring::length(password)) {
+
+		fprintf(stderr,"usage: sqlrpwdenc [-config configfile] "
+			"-id instance -pwdencid passwordencryptionid "
+			"-password password\n");
+		process::exit(1);
+	}
+
+	// open the config file
+	sqlrconfigfile	cfgfl;
+	if (!cfgfl.parse(config,id)) {
+		fprintf(stderr,"SQL Relay instance %s not found in %s\n",
+								id,config);
+		process::exit(1);
+	}
+
+	// initialize the password encryption framework
+	const char	*pwdencs=cfgfl.getPasswordEncryptions();
+	if (!charstring::length(pwdencs)) {
+		fprintf(stderr,"password encryption id %s not found\n",
+								pwdencid);
+		process::exit(1);
+	}
+	sqlrpwdencs	sqlrpe;
+	sqlrpe.loadPasswordEncryptions(pwdencs);
+	sqlrpwdenc	*sqlrp=sqlrpe.getPasswordEncryptionById(pwdencid);
+	if (!sqlrp) {
+		fprintf(stderr,"password encryption id %s not found\n",
+								pwdencid);
+		process::exit(1);
+	}
+
+	// encrypt the password and print the result
+	char	*encryptedpassword=sqlrp->encrypt(password);
+	printf("%s\n",encryptedpassword);
+	delete[] encryptedpassword;
+	process::exit(0);
+}
