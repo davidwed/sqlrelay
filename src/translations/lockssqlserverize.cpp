41267a5312a432f42836e74549c6e31cdb58f6a2
David Muse <david.muse@firstworks.com>
2013-02-13 18:05:29 +0000

    #1517 added lock translation

---
+++ src/translations/lockssqlserverize.cpp
@@ -0,0 +1,128 @@
+// Copyright (c) 1999-2012  David Muse
+// See the file COPYING for more information
+
+#include <sqlrconnection.h>
+#include <sqlrcursor.h>
+#include <sqlparser.h>
+#include <sqltranslation.h>
+#include <debugprint.h>
+
+#ifdef RUDIMENTS_NAMESPACE
+using namespace rudiments;
+#endif
+
+class lockssqlserverize : public sqltranslation {
+	public:
+			lockssqlserverize(sqltranslations *sqlts,
+						xmldomnode *parameters);
+		bool	run(sqlrconnection_svr *sqlrcon,
+					sqlrcursor_svr *sqlrcur,
+					xmldom *querytree);
+};
+
+lockssqlserverize::lockssqlserverize(sqltranslations *sqlts,
+					xmldomnode *parameters) :
+					sqltranslation(sqlts,parameters) {
+}
+
+bool lockssqlserverize::run(sqlrconnection_svr *sqlrcon,
+					sqlrcursor_svr *sqlrcur,
+					xmldom *querytree) {
+
+	xmldomnode	*query=querytree->getRootNode();
+
+	// lock query...
+	xmldomnode	*locknode=query->getFirstTagChild(
+						sqlparser::_lock);
+	if (locknode->isNullNode()) {
+		return false;
+	}
+
+	// table
+	xmldomnode	*tablenode=locknode->getFirstTagChild(
+						sqlparser::_table);
+	if (tablenode->isNullNode()) {
+		return false;
+	}
+
+	// table name
+	xmldomnode	*tablenamedbnode=
+				tablenode->getFirstTagChild(
+					sqlparser::_table_name_database);
+	xmldomnode	*tablenameschemanode=
+				tablenode->getFirstTagChild(
+					sqlparser::_table_name_schema);
+	xmldomnode	*tablenametablenode=
+				tablenode->getFirstTagChild(
+					sqlparser::_table_name_table);
+	if (tablenametablenode->isNullNode()) {
+		return false;
+	}
+	const char	*tablenamedb=
+			tablenamedbnode->getAttributeValue("value");
+	const char	*tablenameschema=
+			tablenameschemanode->getAttributeValue("value");
+	const char	*tablenametable=
+			tablenametablenode->getAttributeValue("value");
+	if (!charstring::length(tablenametable)) {
+		return false;
+	}
+
+	// in
+	xmldomnode	*innode=tablenametablenode->getNextTagSibling(
+							sqlparser::_in_mode);
+	if (innode->isNullNode()) {
+		return false;
+	}
+
+	// lock mode
+	xmldomnode	*lockmodenode=tablenode->getNextTagSibling(
+						sqlparser::_lock_mode);
+	if (lockmodenode->isNullNode()) {
+		return false;
+	}
+	const char	*value=lockmodenode->getAttributeValue("value");
+	bool	exclusive=!charstring::compareIgnoringCase(value,"exclusive");
+	bool	share=!charstring::compareIgnoringCase(value,"share");
+	if (!exclusive && !share) {
+		return false;
+	}
+
+	// mode
+	xmldomnode	*modenode=tablenode->getNextTagSibling(
+						sqlparser::_mode);
+	if (modenode->isNullNode()) {
+		return false;
+	}
+
+	// build a new query using tablockx for exclusive and tablock for share
+	stringbuffer	newquery;
+	newquery.append("select 1 from ");
+	if (charstring::length(tablenamedb)) {
+		newquery.append(tablenamedb)->append('.');
+	}
+	if (charstring::length(tablenameschema)) {
+		newquery.append(tablenameschema)->append('.');
+	}
+	newquery.append(tablenametable);
+	newquery.append(" with ");
+	newquery.append((exclusive)?"(tablockx)":"(tablock)");
+
+	// dump the old query
+	query->deleteChild(locknode);
+
+	// parse and attach the new query
+	sqlparser	sqlp;
+	sqlp.useTree(querytree);
+	sqlp.parse(newquery.getString());
+
+	return true;
+}
+
+extern "C" {
+	sqltranslation	*new_lockssqlserverize(
+					sqltranslations *sqlts,
+					xmldomnode *parameters) {
+		return new lockssqlserverize(sqlts,parameters);
+	}
+}
