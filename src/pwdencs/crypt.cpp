f78a88973a78c226eb0fde44887f420034918990
David Muse <david.muse@firstworks.com>
2013-08-25 02:45:08 +0000

    *** empty log message ***

--- src/pwdencs/crypt.cpp
+++ src/pwdencs/crypt.cpp
@@ -25,7 +25,7 @@ char *crypt_pwdenc::encrypt(const char *value) {
 
 	// the first two characters of the result string
 	// are the salt, so don't include them, if possible
-	char	*encrypted=rudiments::crypt::encrypt(value,
+	char	*encrypted=crypt::encrypt(value,
 					parameters->getAttributeValue("salt"));
 	if (charstring::length(encrypted)<2) {
 		return encrypted;
