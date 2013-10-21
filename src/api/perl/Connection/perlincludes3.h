898dfce239401708254ddadbd2ef3525183cb2e6
David Muse <david.muse@firstworks.com>
2013-10-21 05:14:27 +0000

    *** empty log message ***

--- src/api/perl/Connection/perlincludes3.h
+++ src/api/perl/Connection/perlincludes3.h
@@ -1,4 +1,21 @@
-#include "../../c++/include/sqlrelay/sqlrclient.h"
+/*#include "../../c++/include/sqlrelay/sqlrclient.h"
 #include <EXTERN.h>
 #include <perl.h>
+#include <XSUB.h>*/
+#include "../../c++/include/sqlrelay/sqlrclient.h"
+#include <EXTERN.h>
+extern "C" {
+	/*#ifdef __cplusplus
+		#define __cplusplus_was_set
+		#undef __cplusplus
+	#endif*/
+	#include <perl.h>
+	/*#ifdef __cplusplus_was_set
+		#define __cplusplus
+		#undef __cplusplus_was_set
+	#endif*/
+}
 #include <XSUB.h>
+#ifdef CLASS
+	#undef CLASS
+#endif
--- src/api/perl/Connection/perlincludes4.h
+++ src/api/perl/Connection/perlincludes4.h
@@ -1,7 +1,24 @@
-#include "../../c++/include/sqlrelay/sqlrclient.h"
+/*#include "../../c++/include/sqlrelay/sqlrclient.h"
 #include <EXTERN.h>
 extern "C" {
 	#include <perl.h>
 }
 #include <XSUB.h>
-#undef CLASS
+#undef CLASS*/
+#include "../../c++/include/sqlrelay/sqlrclient.h"
+#include <EXTERN.h>
+extern "C" {
+	/*#ifdef __cplusplus
+		#define __cplusplus_was_set
+		#undef __cplusplus
+	#endif*/
+	#include <perl.h>
+	/*#ifdef __cplusplus_was_set
+		#define __cplusplus
+		#undef __cplusplus_was_set
+	#endif*/
+}
+#include <XSUB.h>
+#ifdef CLASS
+	#undef CLASS
+#endif
