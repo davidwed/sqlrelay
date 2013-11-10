5ddde59452c6fc6f428bbbb9c6a4a570ca229dec
David Muse <david.muse@firstworks.com>
2013-11-10 01:09:00 +0000

    #1964 updated license info

--- src/api/phppdo/pdo_sqlrelay.cpp
+++ src/api/phppdo/pdo_sqlrelay.cpp
@@ -1,20 +1,5 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2013 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author: David Muse <david.muse@firstworks.com>                       |
-  +----------------------------------------------------------------------+
-*/
+// Copyright (c) 2013 David Muse
+// See the file COPYING for more information
 
 #include <config.h>
 #define NEED_IS_BIT_TYPE_CHAR
