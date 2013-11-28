5b14197ad2835051599aa8933c633ffe0fd42d7f
David Muse <david.muse@firstworks.com>
2013-11-28 01:41:07 +0000

    #1993 moved drop-table and session-end queries to after session-end commit/rollback

--- src/server/sqlrcontroller.cpp
+++ src/server/sqlrcontroller.cpp
@@ -2888,25 +2888,19 @@ void sqlrcontroller_svr::endSession() {
 	}
 	logDebugMessage("done aborting all cursors");
 
-	// truncate/drop temp tables
-	// (Do this before running the end-session queries becuase
-	// with oracle, it may be necessary to log out and log back in to
-	// drop a temp table.  With each log-out the session end queries
-	// are run and with each log-in the session start queries are run.)
-	truncateTempTables(cur[0]);
-	dropTempTables(cur[0]);
-
-	sessionEndQueries();
-
 	// must set suspendedsession to false here so resumed sessions won't 
 	// automatically re-suspend
 	suspendedsession=false;
 
-	// if we're faking transaction blocks and the session was ended but we
-	// haven't ended the transaction block, then we need to rollback and
-	// end the block
+	// Run end-of-session rollback or commit before dropping tables and
+	// running session-end-queries.  Some queries, including drop table,
+	// cause an implicit commit.  If we need to rollback, then make sure
+	// that's done first.
 	if (intransactionblock) {
 
+		// if we're faking transaction blocks and the session was ended
+		// but we haven't ended the transaction block, then we need to
+		// rollback and end the block
 		rollback();
 		intransactionblock=false;
 
@@ -2923,6 +2917,17 @@ void sqlrcontroller_svr::endSession() {
 			logDebugMessage("done rolling back...");
 		}
 	}
+
+	// truncate/drop temp tables
+	// (Do this before running the end-session queries becuase
+	// with oracle, it may be necessary to log out and log back in to
+	// drop a temp table.  With each log-out the session end queries
+	// are run and with each log-in the session start queries are run.)
+	truncateTempTables(cur[0]);
+	dropTempTables(cur[0]);
+
+	// run session-end queries
+	sessionEndQueries();
 
 	// reset database/schema
 	if (dbselected) {
