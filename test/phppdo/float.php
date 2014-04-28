bbb8a87e0b1bb67e0a1059be2962fc62b0e67aa7
David Muse <david.muse@firstworks.com>
2014-04-28 01:26:00 +0000

    *** empty log message ***

---
+++ test/phppdo/float.php
@@ -0,0 +1,69 @@
+<html><pre><?php
+# Copyright (c) 2013  David Muse
+# See the file COPYING for more information.
+
+dl("pdo_sqlrelay.so");
+
+	function checkSuccess($value,$success) {
+
+		if ($value==$success) {
+			echo("success ");
+		} else {
+			echo("$value != $success ");
+			echo("failure ");
+			exit(0);
+		}
+	}
+
+	$host="localhost";
+	$port=9000;
+	$socket="/tmp/test.socket";
+	$user="test";
+	$password="test";
+	$dsn = "sqlrelay:host=$host;port=$port;socket=$socket;tries=0;retrytime=1;debug=0";
+
+
+	# instantiation
+	$dbh=new PDO($dsn,$user,$password);
+	if(!$dbh){
+		die("new PDO failed");
+	}
+
+	# drop existing table
+	$dbh->exec("drop table testtable");
+
+	echo("CREATE TEMPTABLE: \n");
+	$dbh->exec("create table testtable (testfloat float, testdouble double)");
+	echo("\n");
+
+	echo("INSERT: \n");
+	checkSuccess($dbh->exec("insert into testtable values (3.14,3.14)"),1);
+	checkSuccess($dbh->exec("insert into testtable values (6.28,6.28)"),1);
+	checkSuccess($dbh->exec("insert into testtable values (9.42,9.42)"),1);
+	echo("\n");
+
+	echo("SELECT: \n");
+	$stmt=$dbh->query("select * from testtable order by testfloat");
+	echo("\n");
+	
+	echo("FIELDS BY INDEX: \n");
+	$result=$stmt->fetch(PDO::FETCH_NUM);
+	checkSuccess($result[0],"3.14");
+	checkSuccess($result[1],"3.14");
+	echo("\n");
+
+	echo("FIELDS BY NAME: \n");
+	$result=$stmt->fetch(PDO::FETCH_ASSOC);
+	checkSuccess($result["testfloat"],"6.28");
+	checkSuccess($result["testdouble"],"6.28");
+	echo("\n");
+
+	echo("FIELDS BY NAME AND INDEX: \n");
+	$result=$stmt->fetch();
+	checkSuccess($result[0],"9.42");
+	checkSuccess($result[1],"9.42");
+	echo("\n");
+
+	$dbh->exec("drop table testtable");
+
+?></pre></html>
