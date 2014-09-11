85e778e1e03a9fcecb277d4350be41e50a02a54e
David Muse <david.muse@firstworks.com>
2014-09-11 00:27:11 +0000

    *** empty log message ***

---
+++ test/phppdo/nulldate.php
@@ -0,0 +1,39 @@
+<html><pre><?php
+
+	dl("pdo_sqlrelay.so");
+
+	# sqlrelay
+	$host="localhost";
+	$port=9000;
+	$socket="/tmp/test.socket";
+	$user="test";
+	$password="test";
+	$dsn="sqlrelay:host=$host;port=$port;socket=$socket;tries=0;retrytime=1;debug=0";
+
+	# mysql
+	#$user="testuser";
+	#$password="testpassword";
+	#$dsn="mysql:host=db64;port=3306;dbname=testdb";
+
+
+	$dbh=new PDO($dsn,$user,$password);
+	if(!$dbh){
+		die("new PDO failed");
+	}
+
+	$dbh->exec("drop table testdate");
+	$dbh->exec("create table testdate (date1 datetime, date2 datetime, date3 datetime, date4 datetime)");
+	$dbh->exec("insert into testdate values (NULL,'','0000-00-00 00:00:00','2001-01-01')");
+
+	$stmt=$dbh->query("select * from testdate");
+	$result=$stmt->fetch(PDO::FETCH_ASSOC);
+
+	echo("\n");
+	print_r($result);
+	echo("\n");
+
+	$stmt=NULL;
+
+	$dbh->exec("drop table testdate");
+
+?></pre></html>
