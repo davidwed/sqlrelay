07a156eb0e8439b6cd412f5cf088da74a096d7ca
David Muse <david.muse@firstworks.com>
2015-02-09 23:58:28 +0000

    *** empty log message ***

--- test/bench/bench.php
+++ test/bench/bench.php
@@ -1,29 +1,32 @@
 <html><pre><?php
-# Copyright (c) 2015  David Muse
-# See the file COPYING for more information.
+	# Copyright (c) 2015  David Muse
+	# See the file COPYING for more information.
 
+	# sqlrelay connection
 	$sqlrelay=array(
-		"dsn" = "sqlrelay:host=sqlrserver;port=9000;socket=/tmp/test.socket;tries=0;retrytime=1;debug=0",
+		"dsn" => "sqlrelay:host=sqlrserver;port=9000;socket=/tmp/test.socket;tries=0;retrytime=1;debug=0",
 		"user" => "test",
 		"password" => "test"
 	);
 
+	# mysql connection
 	$mysql=array(
-		"dsn" = "mysql:host=db64;port=3360;dbname=testdb",
+		"dsn" => "mysql:host=db64;port=3360;dbname=testdb",
 		"user" => "testuser",
 		"password" => "testpassword"
 	);
 
+	# set current configuration
 	$conn=$sqlrelay;
 
 
-	# instantiation
+	# create db connection
 	$dbh=new PDO($conn["dsn"],$conn["user"],$conn["password"]);
 	if(!$dbh){
 		die("new PDO failed");
 	}
 
-	# drop existing table
-	$dbh->exec("drop table testtable");
+	# clean up
+	$dbh=null;
 
 ?></pre></html>
