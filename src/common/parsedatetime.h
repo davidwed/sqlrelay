8362101e2aeb1dda7278ebc334030dc47f2e650f
David Muse <david.muse@firstworks.com>
2015-04-20 20:45:30 +0000

    *** empty log message ***

--- src/common/parsedatetime.h
+++ src/common/parsedatetime.h
@@ -51,7 +51,6 @@ static bool parseDateTime(const char *datetime, bool ddmm, bool yyyyddmm,
 			int16_t *year, int16_t *month, int16_t *day,
 			int16_t *hour, int16_t *minute, int16_t *second,
 			int16_t *fraction) {
-stdoutput.printf("datetime - %s\n",datetime);
 
 	// initialize date/time parts
 	*year=-1;
