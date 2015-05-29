5fad186b020fde4c897aac41811b9680fd3b9adc
David Muse <david.muse@firstworks.com>
2015-05-29 16:31:36 +0000

    #3118 added code to get prefix from windows registry and fall back to defaults

--- src/util/sqlrpaths.cpp
+++ src/util/sqlrpaths.cpp
@@ -8,21 +8,63 @@
 #include <rudiments/stringbuffer.h>
 
 #ifdef _WIN32
-	#define LOCALSTATEDIR "C:\\Program Files\\Firstworks\\var\\"
-	#define SYSCONFDIR "C:\\Program Files\\Firstworks\\etc\\"
-	#define LIBEXECDIR "C:\\Program Files\\Firstworks\\libexec\\sqlrelay\\"
+	#include <windows.h>
 #endif
 
 sqlrpaths::sqlrpaths(sqlrcmdline *cmdl) {
+
+	stringbuffer	scratch;
+
+	char	*defaultlocalstatedir;
+	char	*sysconfdir;
+
+#ifdef _WIN32
+
+	// get prefix from HKEY_LOCAL_MACHINE\SOFTWARE\SQLRelay\prefix
+	HKEY	hkey;
+	char	prefix[512];
+	DWORD	prefixsize=sizeof(prefix);
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+				"SOFTWARE\\SQLRelay",
+				0,KEY_READ,&hkey)!=ERROR_SUCCESS ||
+		RegQueryValueEx(hkey,"prefix",0,NULL,
+				prefix,&prefixsize)!=ERROR_SUCCESS) {
+
+		// fall back to defaults
+		if (sizeof(long)==8) {
+			charstring::copy(prefix,
+					"C:\\Program Files\\Firstworks");
+		} else {
+			charstring::copy(prefix,
+					"C:\\Program Files (x86)\\Firstworks");
+		}
+	}
+
+	// build default localstatedir
+	scratch.append(prefix)->append("\\var\\");
+	defaultlocalstatedir=scratch.detachString();
+
+	// sysconfdir
+	scratch.append(prefix)->append("\\etc\\");
+	sysconfdir=scratch.detachString();
+
+	// libexecdir
+	scratch.append(prefix)->append("\\libexec\\");
+	libexecdir=scratch.detachString();
+
+#else
+	defaultlocalstatedir=charstring::duplicate(LOCALSTATEDIR);
+	sysconfdir=charstring::duplicate(SYSCONFDIR);
+	libexecdir=charstring::duplicate(LIBEXECDIR);
+#endif
 
 	char	slash=sys::getDirectorySeparator();
 
 	localstatedir=cmdl->getLocalStateDir();
 	if (!cmdl->getLocalStateDir()[0]) {
-		localstatedir=LOCALSTATEDIR;
+		localstatedir=defaultlocalstatedir;
 	}
 
-	stringbuffer	scratch;
 	scratch.append(localstatedir)->append("sqlrelay")->append(slash);
 	char	*lsdir=scratch.detachString();
 
@@ -51,8 +93,6 @@ sqlrpaths::sqlrpaths(sqlrcmdline *cmdl) {
 	scratch.append(lsdir)->append("cache")->append(slash);
 	cachedir=scratch.detachString();
 
-	const char	*sysconfdir=SYSCONFDIR;
-
 	scratch.append(sysconfdir)->append("sqlrelay.conf");
 	defaultconfigfile=scratch.detachString();
 
@@ -65,7 +105,9 @@ sqlrpaths::sqlrpaths(sqlrcmdline *cmdl) {
 		configfile=defaultconfigfile;
 	}
 
-	libexecdir=LIBEXECDIR;
+	delete[] defaultlocalstatedir;
+	delete[] sysconfdir;
+	delete[] libexecdir;
 }
 
 sqlrpaths::~sqlrpaths() {
