5544ddae2ab0fc80fda12fffbe149405c92c88d9
David Muse <david.muse@firstworks.com>
2015-06-16 04:45:34 +0000

    *** empty log message ***

--- src/connections/postgresqlconnection.cpp
+++ src/connections/postgresqlconnection.cpp
@@ -201,9 +201,6 @@ void postgresqlconnection::handleConnectString() {
 	cont->setUser(cont->getConnectStringValue("user"));
 	cont->setPassword(cont->getConnectStringValue("password"));
 	sslmode=cont->getConnectStringValue("sslmode");
-	if (!charstring::length(sslmode)) {
-		sslmode="disable";
-	}
 	const char	*typemang=cont->getConnectStringValue("typemangling");
 	if (!typemang ||!charstring::compareIgnoringCase(typemang,"no")) {
 		typemangling=0;
@@ -253,7 +250,7 @@ bool postgresqlconnection::logIn(const char **error, const char **warning) {
 	if (db && db[0]) {
 		conninfo.append(" dbname=")->append(db);
 	}
-	if (sslmode && sslmode[0]) {
+	if (sslmode && sslmode[0] && charstring::compare(sslmode,"disable")) {
 		conninfo.append(" sslmode=")->append(sslmode);
 	}
 	pgconn=PQconnectdb(conninfo.getString());
