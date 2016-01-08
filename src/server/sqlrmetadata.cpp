8afcd1f83a78fed1a89143159df03ae54609508e
David Muse <david.muse@firstworks.com>
2016-01-08 22:44:33 +0000

    *** empty log message ***

---
+++ src/server/sqlrmetadata.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 1999-2014  David Muse
+// All rights reserved
+
+#include <sqlrelay/sqlrserver.h>
+
+sqlrmetadata::sqlrmetadata(bool debug) {
+	this->debug=debug;
+}
+
+sqlrmetadata::~sqlrmetadata() {
+}
