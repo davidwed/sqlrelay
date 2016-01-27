49661aee4d57255023f62b63ae975cabc4349d55
David Muse <david.muse@firstworks.com>
2016-01-27 06:49:39 +0000

    *** empty log message ***

--- src/api/c++/sqlrelay/private/sqlrclientincludes.h
+++ src/api/c++/sqlrelay/private/sqlrclientincludes.h
@@ -1,7 +1,6 @@
 // Copyright (c) 1999-2001  David Muse
 // See the COPYING file for more information.
 
-
 #include <rudiments/inetsocketclient.h>
 #include <rudiments/unixsocketclient.h>
 #include <rudiments/dynamicarray.h>
@@ -9,7 +8,15 @@
 #include <rudiments/stringbuffer.h>
 #include <rudiments/file.h>
 
-#include <sqlrelay/private/dll.h>
+#ifdef _WIN32
+	#ifdef LIBSQLRCLIENT_EXPORTS
+		#define SQLRCLIENT_DLLSPEC __declspec(dllexport)
+	#else
+		#define SQLRCLIENT_DLLSPEC __declspec(dllimport)
+	#endif
+#else
+	#define SQLRCLIENT_DLLSPEC
+#endif
 
 enum bindvartype_t {
 	BINDVARTYPE_NULL=0,
