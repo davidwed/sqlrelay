1308ab3d648d2a9b7b45fead0404919b7f57898d
David Muse <david.muse@firstworks.com>
2019-07-03 13:40:02 -0400

    added simple mssql lob/date test

---
+++ test/phppdo/mssql.php
@@ -0,0 +1,28 @@
+<?php
+# Copyright (c) 1999-2018 David Muse
+# See the file COPYING for more information.
+
+	$host="sqlrelay";
+	$port=8000;
+	$socket="/tmp/test.socket";
+	$user="test";
+	$password="test";
+	$dsn = "sqlrelay:host=$host;port=$port;socket=$socket;tries=0;retrytime=1;debug=1";
+
+	$dbh=new PDO($dsn,$user,$password);
+	$stmt=$dbh->query("select ".
+		"cast('1958-01-15 01:02:03' as datetime) as datetimevalue, ".
+		"cast('1958-01-15' as date) as datevalue, ".
+		"cast('<document><element/></document>' as xml) as xmlvalue, ".
+		"cast('FOOB10' as text) as textvalue, ".
+		"cast('FOOB11' as ntext) as ntextvalue, ".
+		"cast('FOOB11' as image) as imagevalue");
+	$result=$stmt->fetch(PDO::FETCH_NUM);
+	echo("$result[0]\n");
+	echo("$result[1]\n");
+	echo(stream_get_contents($result[2])."\n");
+	echo(stream_get_contents($result[3])."\n");
+	echo(stream_get_contents($result[4])."\n");
+	echo(stream_get_contents($result[5])."\n");
+	echo("\n");		
+?>
