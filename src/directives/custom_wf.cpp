87ab626c8e762478c4415b40d12ff20c31110e58
David Muse <david.muse@firstworks.com>
2019-10-25 14:29:27 -0400

    split sqlrelay-crash into its own module

--- src/directives/custom_wf.cpp
+++ src/directives/custom_wf.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 1999-2018 David Muse
+// Copyright (c) 1999-2019 David Muse
 // See the file COPYING for more information
 
 #include <sqlrelay/sqlrserver.h>
@@ -19,7 +19,6 @@ class SQLRSERVER_DLLSPEC sqlrdirective_custom_wf : public sqlrdirective {
 		void	parseDirective(sqlrservercursor *sqlrcur,
 					const char *directivestart,
 					uint32_t length);
-		void	crashmeTest(int32_t iargument);
 
 		sqlrservercontroller	*cont;
 
@@ -44,8 +43,6 @@ sqlrdirective_custom_wf::sqlrdirective_custom_wf(
 #define KEYWORD_SQLEXECDIRECT "sqlexecdirect"
 #define KEYWORD_QUERYTIMEOUT "querytimeout:"
 #define KEYWORD_SQLPREPARE "sqlprepare"
-#define KEYWORD_SQLRELAY_CRASH "sqlrelay-crash"
-#define KEYWORD_SQLRELAY_CRASH_ARG "sqlrelay-crash:"
 #define MARKER_ODBC_RPC '{'
 
 bool sqlrdirective_custom_wf::run(sqlrserverconnection *sqlrcon,
@@ -116,46 +113,14 @@ void sqlrdirective_custom_wf::parseDirective(
 		sqlrcur->setExecuteDirect(false);
 	}
 
-	if (!charstring::compare(directivestart,
-				KEYWORD_SQLRELAY_CRASH,
-				length)) {
-		if (debug) {
-			stdoutput.printf("%s...\n",KEYWORD_SQLRELAY_CRASH);
-		}
-		crashmeTest(0);
-		return;
-	}
-
-	int32_t		argumentsize;
-	const char	*argument;
-	int32_t		iargument=0;
-
-	if ((length>charstring::length(KEYWORD_SQLRELAY_CRASH_ARG)) &&
-		(!charstring::compare(directivestart,
-			KEYWORD_SQLRELAY_CRASH_ARG,
-			charstring::length(KEYWORD_SQLRELAY_CRASH_ARG)))) {
-
-		argumentsize=length-
-				charstring::length(KEYWORD_SQLRELAY_CRASH_ARG);
-		argument=&directivestart[
-				charstring::length(KEYWORD_SQLRELAY_CRASH_ARG)];
-
-		if (charstring::isInteger(argument,argumentsize)) {
-			iargument=charstring::toInteger(argument);
-		}
-
-		crashmeTest(iargument);
-		return;
-	}
-
 	if ((length>charstring::length(KEYWORD_QUERYTIMEOUT)) &&
 		(!charstring::compare(directivestart,
 				KEYWORD_QUERYTIMEOUT,
 				charstring::length(KEYWORD_QUERYTIMEOUT)))) {
 
-		argumentsize=length-
+		int32_t		argumentsize=length-
 				charstring::length(KEYWORD_QUERYTIMEOUT);
-		argument=&directivestart[
+		const char	*argument=&directivestart[
 				charstring::length(KEYWORD_QUERYTIMEOUT)];
 
 		if (charstring::isInteger(argument,argumentsize)) {
@@ -175,54 +140,6 @@ void sqlrdirective_custom_wf::parseDirective(
 					KEYWORD_QUERYTIMEOUT);
 		}
 		return;
-	}
-}
-
-void sqlrdirective_custom_wf::crashmeTest(int32_t iargument) {
-	debugFunction();
-
-	char	*some_ptr=(char *)NULL;
-
-	if (iargument==0) {
-		// assignment to NULL pointer
-		if (debug) {
-			stdoutput.printf("%s:assignment to NULL ptr...\n",
-					KEYWORD_SQLRELAY_CRASH_ARG);
-		}
-		some_ptr[0]=1;
-	} else if (iargument==1) {
-		// delete NULL pointer
-		if (debug) {
-			stdoutput.printf("%s:delete NULL ptr...\n",
-					KEYWORD_SQLRELAY_CRASH_ARG);
-		}
-		delete[] some_ptr;
-	} else if (iargument==2) {
-		// double free
-		if (debug) {
-			stdoutput.printf("%s:double free...\n",
-					KEYWORD_SQLRELAY_CRASH_ARG);
-		}
-		some_ptr=new char[100];
-		delete[] some_ptr;
-		delete[] some_ptr;
-	} else if (iargument==3) {
-		// accessing NULL pointer
-		if (debug) {
-			stdoutput.printf("%s:access NULL ptr...\n",
-					KEYWORD_SQLRELAY_CRASH_ARG);
-		}
-		some_ptr[0]=(memchr(some_ptr,25,30))?1:2;
-	} else if (iargument==4) {
-		// accessing NULL pointer 
-		if (debug) {
-			stdoutput.printf("%s:assign and access NULL ptr...\n",
-					KEYWORD_SQLRELAY_CRASH_ARG);
-		}
-		some_ptr[0]=some_ptr[10];
-	} else if (debug) {
-		stdoutput.printf("%s:...bad argument...\n",
-				KEYWORD_SQLRELAY_CRASH_ARG);
 	}
 }
 
