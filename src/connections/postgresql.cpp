00719a063fb8944c44144d5ff1e5481af3cbb287
David Muse <david.muse@firstworks.com>
2020-11-03 22:26:48 -0500

    fixed ncols=0 issue caused by the fix to #5909

--- src/connections/postgresql.cpp
+++ src/connections/postgresql.cpp
@@ -779,6 +779,7 @@ postgresqlcursor::~postgresqlcursor() {
 		(defined(HAVE_POSTGRESQL_PQSENDQUERYPREPARED) && \
 		defined(HAVE_POSTGRESQL_PQSETSINGLEROWMODE))
 bool postgresqlcursor::prepareQuery(const char *query, uint32_t length) {
+stdoutput.printf("%s - prepare\n",cursorid);
 
 	// initialize the column count
 	ncols=0;
@@ -850,6 +851,7 @@ bool postgresqlcursor::inputBind(const char *variable,
 					const char *value, 
 					uint32_t valuesize,
 					int16_t *isnull) {
+stdoutput.printf("%s - inputbind - %s=%s\n",cursorid,variable,value);
 
 	// "variable" should be something like ?1,?2,?3, etc.
 	// If it's something like ?var1,?var2,?var3, etc. then it'll be
@@ -1017,6 +1019,7 @@ void postgresqlcursor::encodeBlob(stringbuffer *buffer,
 }
 
 bool postgresqlcursor::executeQuery(const char *query, uint32_t length) {
+stdoutput.printf("%s - execute\n",cursorid);
 
 	// initialize the row counts
 	nrows=0;
@@ -1082,14 +1085,21 @@ bool postgresqlcursor::executeQuery(const char *query, uint32_t length) {
 		return false;
 	}
 
-#if !((defined(HAVE_POSTGRESQL_PQEXECPREPARED) && \
+	// NOTE: this is a bit of a kludge.
+	//
+	// As this is set in prepareQuery(), originally, we only did this here
+	// if we don't support prepared queries.  However, since it's set to 0
+	// in closeResultSet() (see long note there as to why), we must reset
+	// it to the correct count here so that ncols will be correct for
+	// reexecuted queries.
+/*#if !((defined(HAVE_POSTGRESQL_PQEXECPREPARED) && \
 		defined(HAVE_POSTGRESQL_PQPREPARE)) || \
 		(defined(HAVE_POSTGRESQL_PQSENDQUERYPREPARED) && \
 		defined(HAVE_POSTGRESQL_PQSETSINGLEROWMODE))) || \
-		!(defined(HAVE_POSTGRESQL_PQDESCRIBEPREPARED))
+		!(defined(HAVE_POSTGRESQL_PQDESCRIBEPREPARED))*/
 	// get the col count
 	ncols=PQnfields(pgresult);
-#endif
+//#endif
 
 	// validate column count
 	uint32_t	maxcolumncount=conn->cont->getMaxColumnCount();
@@ -1546,6 +1556,7 @@ void postgresqlcursor::getField(uint32_t col,
 }
 
 void postgresqlcursor::closeResultSet() {
+stdoutput.printf("%s - close result set\n",cursorid);
 
 #if (defined(HAVE_POSTGRESQL_PQEXECPREPARED) && \
 		defined(HAVE_POSTGRESQL_PQPREPARE)) || \
