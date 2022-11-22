3f79b5613b0e1bc0ab22c8063e77d1e9c3862b92
David Muse <david.muse@firstworks.com>
2022-11-22 13:42:10 -0500

    crypt -> des

--- src/pwdencs/des.cpp
+++ src/pwdencs/des.cpp
@@ -3,35 +3,35 @@
 
 #include <sqlrelay/sqlrserver.h>
 #include <rudiments/charstring.h>
-#include <rudiments/crypt.h>
+#include <rudiments/des.h>
 
-class SQLRSERVER_DLLSPEC sqlrpwdenc_crypt : public sqlrpwdenc {
+class SQLRSERVER_DLLSPEC sqlrpwdenc_des : public sqlrpwdenc {
 	public:
-			sqlrpwdenc_crypt(domnode *parameters, bool debug);
+			sqlrpwdenc_des(domnode *parameters, bool debug);
 		bool	oneWay();
 		char	*encrypt(const char *value);
 
-		class crypt	c;
+		class des	d;
 };
 
-sqlrpwdenc_crypt::sqlrpwdenc_crypt(domnode *parameters, bool debug) :
+sqlrpwdenc_des::sqlrpwdenc_des(domnode *parameters, bool debug) :
 						sqlrpwdenc(parameters,debug) {
 }
 
-bool sqlrpwdenc_crypt::oneWay() {
+bool sqlrpwdenc_des::oneWay() {
 	return true;
 }
 
-char *sqlrpwdenc_crypt::encrypt(const char *value) {
+char *sqlrpwdenc_des::encrypt(const char *value) {
 
-	c.setIv((const unsigned char *)
+	d.setIv((const unsigned char *)
 			getParameters()->getAttributeValue("salt"),
-			c.getIvSize());
+			d.getIvSize());
 
-	c.append((const unsigned char *)value,
+	d.append((const unsigned char *)value,
 			charstring::length(value));
 
-	const char	*encrypted=(const char *)c.getEncryptedData();
+	const char	*encrypted=(const char *)d.getEncryptedData();
 
 	// the first two characters of the result string are the salt,
 	// so don't include them in the result, if possible
@@ -41,9 +41,9 @@ char *sqlrpwdenc_crypt::encrypt(const char *value) {
 }
 
 extern "C" {
-	SQLRSERVER_DLLSPEC sqlrpwdenc *new_sqlrpwdenc_crypt(
+	SQLRSERVER_DLLSPEC sqlrpwdenc *new_sqlrpwdenc_des(
 						domnode *parameters,
 						bool debug) {
-		return new sqlrpwdenc_crypt(parameters,debug);
+		return new sqlrpwdenc_des(parameters,debug);
 	}
 }
